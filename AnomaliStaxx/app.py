32ffd9a255fe9108e90f61db431411fbfe538919
Richard Hope <Richard.Hope@asx.com.au>
2019-01-10 10:36:44 +1100

    Virus Total hash search

--- AnomaliStaxx/app.py
+++ AnomaliStaxx/app.py
@@ -2,7 +2,7 @@ import logging
 from apps import App, action
 import requests
 
-
+requests.packages.urllib3.disable_warnings()
 
 logger = logging.getLogger(__name__)
 
@@ -11,8 +11,21 @@ class AnomaliStaxx(App):
 
     def __init__(self, name, device, context):
         App.__init__(self, name, device, context)
+        self.base_url = 'https://10.170.27.107:8080/api/v1/'
+        self.headers = {'content-type': 'application/json'}
         self.api_key = self.device_fields['key']
+
+    def authenticate(user, password):
+        url = BASE_URL + 'login'
+        data  = json.dumps({'username':user, 'password':password})
+        request = requests.post(url, data=data, headers=HEADERS, verify=False)
+        if request.status_code == 200:
+            return request.json()['token_id']
 
     @action
     def export_indicators(self, search=None proxy=None):
-        pass
+        url = BASE_URL + 'intelligence'
+        data = json.dumps({'token':token, 'query':query, 'type':'json'})
+        request = requests.post(url, data=data, headers=HEADERS, verify=False)
+        if request.status_code == 200:
+            return request.json
---
+++ VirusTotal/app.py
@@ -0,0 +1,21 @@
+import logging
+from apps import App, action
+import requests
+
+logger = logging.getLogger(__name__)
+
+class VirusTotal(App):
+    def __init__(self, name, device, context):
+        App.__init__(self, name, device, context)
+        self.url = 'https://www.virustotal.com/vtapi/v2/file/report'
+        self.proxy = {
+                        'http': 'http://%s:%s' % (self.device.proxy_address, self.device.proxy_port),
+                        'https': 'https://%s:%s' % (self.device.proxy_address, self.device.proxy_port),
+                    }
+        self.api_key = self.device_fields['api_key']
+
+    @action
+    def search_hash(self, file_hash=None):
+        params = {'apikey': self.api_key, 'resource': file_hash}
+        response = requests.get(self.url, params=params, proxies=self.proxy).json()
+        return response

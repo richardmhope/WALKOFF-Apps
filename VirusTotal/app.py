9a27e9a42887a53de4c9ef8bc51cd6e75602bb99
Richard Hope <Richard.Hope@asx.com.au>
2019-01-10 12:55:33 +1100

    add proxy back

--- VirusTotal/app.py
+++ VirusTotal/app.py
@@ -9,9 +9,12 @@ class VirusTotal(App):
         App.__init__(self, name, device, context)
         self.url = 'https://www.virustotal.com/vtapi/v2/file/report'
         
-        print(self.device_fields)
-        self.proxy = None
-        self.api_key = self.device_fields['api_key']
+        if (self.device_fields['proxy_address'] and self.device_fields['proxy_port']):
+            self.proxy = {
+                        'http': 'http://%s:%s' % (self.device_fields['proxy_address'], self.device_fields['proxy_port']),
+                        'https': 'https://%s:%s' % (self.device_fields['proxy_address'], self.device_fields['proxy_port']),
+                    }
+        self.api_key = self.device.get_encrypted_field('api_key')
 
     @action
     def search_hash(self, file_hash):

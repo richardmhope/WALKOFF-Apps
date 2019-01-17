af2bf428bbf708c03e78240758d970c3c9bb2d58
Richard Hope <Richard.Hope@asx.com.au>
2019-01-17 16:10:16 +1100

    URL Scanning

--- VirusTotal/app.py
+++ VirusTotal/app.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 class VirusTotal(App):
     def __init__(self, name, device, context):
         App.__init__(self, name, device, context)
-        self.url = 'https://www.virustotal.com/vtapi/v2/file/report'
+        self.base_url = 'https://www.virustotal.com/vtapi/v2/'
         self.proxy = None
         if (self.device_fields['proxy_address'] and self.device_fields['proxy_port']):
             self.proxy = {
@@ -18,6 +18,28 @@ class VirusTotal(App):
 
     @action
     def search_hash(self, file_hash):
+        url = self.base_url + 'file/report'
         params = {'apikey': self.api_key, 'resource': file_hash}
-        response = requests.get(self.url, params=params, proxies=self.proxy).json()
+        response = requests.get(url, params=params, proxies=self.proxy).json()
+        return response
+
+    @action
+    def search_ip(self, ip):
+        url = self.base_url + 'ip-address/report'
+        params = {'apikey': self.api_key, 'ip': ip}
+        response = requests.get(url, params=params, proxies=self.proxy).json()
+        return response
+
+    @action
+    def search_domain(self, domain):
+        url = self.base_url + 'domain/report'
+        params = {'apikey': self.api_key, 'domain': domain}
+        response = requests.get(url, params=params, proxies=self.proxy).json()
+        return response
+
+    @action
+    def scan_url(self, url):
+        url = self.base_url + 'url/scan'
+        params = {'apikey': self.api_key, 'url': url}
+        response = requests.get(url, params=params, proxies=self.proxy).json()
         return response

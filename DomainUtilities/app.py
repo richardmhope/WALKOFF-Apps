9a25780e1d2a224d5af0887bdefc18265f2c2b66
Richard Hope <Richard.Hope@asx.com.au>
2019-01-22 14:59:55 +1100

    Remove App class to allow for no device

--- DomainUtilities/app.py
+++ DomainUtilities/app.py
@@ -1,34 +1,26 @@
 import re
 import time
-import logging
 import socket
 
 import tldextract
 
-from apps import App, action
-
-logger = logging.getLogger(__name__)
-
-class DomainUtilities(App):
-    def __init__(self, name, device, context):
-        App.__init__(self, name, device, context)
+from apps import action
 
 
-    @action
-    def extract_urls_from_text(self, text):
-        return re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
+@action
+def extract_urls_from_text(text):
+    return re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
 
-    @action
-    def hostname_from_url(self, url):
-        extracted = tldextract.extract(url)
-        return '.'.join(extracted)
+@action
+def hostname_from_url(url):
+    extracted = tldextract.extract(url)
+    return '.'.join(extracted)
 
-    
-    @action
-    def tld_from_domain(self, domain):
-        return tldextract.extract(domain).suffix
+@action
+def tld_from_domain(domain):
+    return tldextract.extract(domain).suffix
 
-    @action
-    def resolve_ip_for_host(self, hostname):
-        return socket.gethostbyname(hostname)
+@action
+def resolve_ip_for_host(hostname):
+    return socket.gethostbyname(hostname)
     

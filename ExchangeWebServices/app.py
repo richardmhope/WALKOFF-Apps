a462aca085b7b727269e53622676743cfc525fc2
Richard Hope <Richard.Hope@asx.com.au>
2019-02-07 21:19:14 +1100

    Attachment handling

--- ExchangeWebServices/app.py
+++ ExchangeWebServices/app.py
@@ -1,4 +1,5 @@
-from exchangelib import Credentials, Account, Configuration, DELEGATE
+from apps import App, action
+from exchangelib import Credentials, Account, Configuration, DELEGATE, ItemAttachment, Message
 from exchangelib.protocol import NoVerifyHTTPAdapter, BaseProtocol
 import os
 
@@ -8,13 +9,21 @@ BaseProtocol.HTTP_ADAPTER_CLS = NoVerifyHTTPAdapter
 class ExchangeWebServices(App):
     def __init__(self, name, device, context):
         App.__init__(self, name, device, context)
-        self.credentials = Credentials(self.device_fields['username'], self.device.get_encrypted_field('password'))
-        self.config = Configuration(self.device_fields['ews_host'], credentials=self.credentials)
-        self.account = Account(primary_smtp_address=self.device_fields['inbox'], config=config,autodiscover=False, access_type=DELEGATE)
+        self.username = self.device_fields['username']
+        self.password = self.device.get_encrypted_field('password')
+        self.host = self.device_fields['host']
+        self.inbox = self.device_fields['inbox']
+
+        self.credentials = Credentials(self.username, self.password)
+        self.config = Configuration(server=self.host, credentials=self.credentials)
+        
+        # Setting this here causes an error, it needs to be in each action because reasons...
+        #self.account = Account(primary_smtp_address=self.inbox, config=self.config, autodiscover=False, access_type=DELEGATE)
     
     @action
     def read_message(self, item_id):
-        email = self.account.inbox.get(item_id=item_id)
+        account = Account(primary_smtp_address=self.inbox, config=self.config, autodiscover=False, access_type=DELEGATE)
+        email = account.inbox.get(item_id=item_id)
         return {
             'item_id': email.item_id,
             'subject': email.subject,
@@ -26,33 +35,69 @@ class ExchangeWebServices(App):
 
     @action
     def move_message(self, item_id, folder):
-        email = email = self.account.inbox.get(item_id=item_id)
+        account = Account(primary_smtp_address=self.inbox, config=self.config, autodiscover=False, access_type=DELEGATE)
+        email = account.inbox.get(item_id=item_id)
         to_folder = self.account.inbox / folder
         email.move(to_folder)
         return 'Successfully moved email to %s' % (folder)
 
     @action
     def delete_message(self, item_id):
-        email = self.account.inbox.get(item_id=item_id)
+        account = Account(primary_smtp_address=self.inbox, config=self.config, autodiscover=False, access_type=DELEGATE)
+        email = account.inbox.get(item_id=item_id)
         email.move_to_trash()
         return 'Successfully moved message to Deleted Items'
 
     @action
     def hard_delete_message(self, item_id):
-        email = self.account.inbox.get(item_id=item_id)
+        account = Account(primary_smtp_address=self.inbox, config=self.config, autodiscover=False, access_type=DELEGATE)
+        email = account.inbox.get(item_id=item_id)
         email.delete()
-        return 'Successfully deleted message'2
+        return 'Successfully deleted message'
 
     @action
     def categorize_message(self, item_id, categories):
-        email = self.account.inbox.get(item_id=item_id)
+        account = Account(primary_smtp_address=self.inbox, config=self.config, autodiscover=False, access_type=DELEGATE)
+        email = account.inbox.get(item_id=item_id)
         email.categories = categories
         email.save()
         return 'Email categorized successfully'
 
     @action
+    def get_attachment_count(self, item_id):
+        account = Account(primary_smtp_address=self.inbox, config=self.config, autodiscover=False, access_type=DELEGATE)
+        email = account.inbox.get(item_id=item_id)
+        return len(email.attachments)
+    
+    @action
+    def attachment_is_message(self, item_id):
+        account = Account(primary_smtp_address=self.inbox, config=self.config, autodiscover=False, access_type=DELEGATE)
+        email = account.inbox.get(item_id=item_id)
+        attachment = email.attachments[0]
+        if isinstance(attachment, ItemAttachment):
+            if isinstance(attachment.item, Message):
+                return True
+        else:
+            return False
+
+    @action 
+    def get_message_attachment(self, item_id):
+        account = Account(primary_smtp_address=self.inbox, config=self.config, autodiscover=False, access_type=DELEGATE)
+        email = account.inbox.get(item_id=item_id)
+        attachment = email.attachments[0]
+        return {
+            'subject': attachment.item.subject,
+            'sender': attachment.item.sender,
+            'body': attachment.item.body,
+            'categories': attachment.item.categories,
+            'headers': attachment.item.headers
+        }
+
+
+    @action
     def get_message_attachments(self, item_id):
-        email = self.account.inbox.get(item_id=item_id)
+        account = Account(primary_smtp_address=self.inbox, config=self.config, autodiscover=False, access_type=DELEGATE)
+        email = account.inbox.get(item_id=item_id)
         for attachment in email.attachments:
             print(attachment.content)
 
@@ -62,22 +107,25 @@ class ExchangeWebServices(App):
 
     @action
     def get_inbox_messages(self):
-    messages = []
-    for item in self.account.inbox.all().order_by('-datetime_received'):
-        messages.append({
-            'item_id': item.item_id,
-            'subject': item.subject,
-            'sender': item.sender,
-            'body': item.body
-        })
-    return messages
+        account = Account(primary_smtp_address=self.inbox, config=self.config, autodiscover=False, access_type=DELEGATE)
+        messages = []
+        for item in account.inbox.all().order_by('-datetime_received'):
+            messages.append({
+                'item_id': item.item_id,
+                'subject': item.subject,
+                'sender': item.sender,
+                'body': item.body
+            })
+        return messages
 
     @action
-    def get_oldest_inbox_message(self)
-        email = self.account.inbox.all().order_by('datetime_received')[0]
+    def get_oldest_inbox_message(self):
+        account = Account(primary_smtp_address=self.inbox, config=self.config, autodiscover=False, access_type=DELEGATE)
+        email = account.inbox.all().order_by('datetime_received')[0]
         return email.item_id
 
     @action
-    def get_newest_inbox_message(self)
-        email = self.account.inbox.all().order_by('-datetime_received')[0]
+    def get_newest_inbox_message(self):
+        account = Account(primary_smtp_address=self.inbox, config=self.config, autodiscover=False, access_type=DELEGATE)
+        email = account.inbox.all().order_by('-datetime_received')[0]
         return email.item_id
